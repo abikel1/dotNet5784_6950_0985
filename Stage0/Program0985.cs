570ba7bc8bf8e4beb7200cd1c1d4895b49a332b4
abikel1 <abikel@g.jct.ac.il>
2023-10-19 16:48:32 +0300

    Initial Code + Welcome functions

---
+++ Stage0/Program0985.cs
@@ -0,0 +1,17 @@
+﻿partial class Program
+{
+    private static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+        Welcome0985();
+        Welcome6950();
+        Console.ReadKey();
+    }
+    static partial void Welcome6950();
+    private static void Welcome0985()
+    {
+        Console.WriteLine("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine(name + ", welcome to my first consile application");
+    }
+}
