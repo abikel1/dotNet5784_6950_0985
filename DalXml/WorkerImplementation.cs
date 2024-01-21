4223a0a0faef90fe4b581780c5aef646fb3ba2cd
ashenker1 <ashenker@g.jct.ac.il>
2024-01-21 16:26:14 +0200

    stage 3

---
+++ DalXml/WorkerImplementation.cs
@@ -0,0 +1,39 @@
+﻿
+namespace Dal;
+using DalApi;
+using DO;
+
+internal class WorkerImplementation:IWorker
+{
+    readonly string s_workers_xml = "workers";
+
+    public int Create(Worker item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Worker? Read(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Worker? Read(Func<Worker, bool> filter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Worker?> ReadAll(Func<Worker, bool>? filter = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Worker item)
+    {
+        throw new NotImplementedException();
+    }
+}
