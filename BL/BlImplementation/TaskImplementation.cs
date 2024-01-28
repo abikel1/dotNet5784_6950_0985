be13449ccd7744393afdbf2b1d858d6e06aa5536
ashenker1 <ashenker@g.jct.ac.il>
2024-01-28 16:28:09 +0200

    Stage 4

--- BL/BlImplementation/TaskImplementation.cs
+++ BL/BlImplementation/TaskImplementation.cs
@@ -1,7 +1,38 @@
 ﻿
 
+using BlApi;
+
 namespace BlImplementation;
 
-internal class TaskImplementation
+internal class TaskImplementation : ITask
 {
+    public void AddTask(BO.Task task)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<BO.Task> ReadTasks(Func<BO.Task, bool>? filter = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveTask(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public BO.Task TaskDetails(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateBeginTask(int id, DateTime date)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateTask(BO.Task task)
+    {
+        throw new NotImplementedException();
+    }
 }
--- BL/BlImplementation/WorkerImplementation.cs
+++ BL/BlImplementation/WorkerImplementation.cs
@@ -1,5 +1,33 @@
-﻿namespace BlImplementation;
-using BlApi;
-internal class WorkerImplementation
+﻿using BlApi;
+using BO;
+
+namespace BlImplementation;
+
+
+internal class WorkerImplementation : IWorker
 {
+    public void AddWorker(Worker worker)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Worker> ReadWorkers(Func<Worker, bool>? filter = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveWorker(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateWorker(Worker worker)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Worker WorkerDetails(int id)
+    {
+        throw new NotImplementedException();
+    }
 }
