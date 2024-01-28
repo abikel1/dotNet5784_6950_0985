4da5cc6e43bc79a1a7b1fd799361b3c2cf719886
abikel1 <abikel@g.jct.ac.il>
2024-01-28 16:12:25 +0200

    stage 4

--- BL/BlImplementation/WorkerImplementation.cs
+++ BL/BlImplementation/WorkerImplementation.cs
@@ -1,6 +1,33 @@
 ﻿namespace BlImplementation;
 using BlApi;
-internal class WorkerImplementation:IWorker
-{
+using BO;
+using System;
+using System.Collections.Generic;
 
+internal class WorkerImplementation : IWorker
+{
+    public void AddWorker(Worker worker)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Worker> ReadWorkers(Func<Worker, bool>? filter = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveWorker(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateWorker(Worker worker)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Worker WorkerDetails(int id)
+    {
+        throw new NotImplementedException();
+    }
 }
